9fb9133181eba4360902219f67a43f265c9c2077
Rushikesh Gore <rushikeshagore@gmail.com>
2021-01-11 18:46:27 +0530

    Internationalization
    using SassionLocaleResolver

--- src/main/java/com/lotusstack/webservices/WebServicesApplication.java
+++ src/main/java/com/lotusstack/webservices/WebServicesApplication.java
@@ -2,12 +2,31 @@ package com.lotusstack.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class WebServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesApplication.class, args);
+    }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource resourceBundleMessageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("messages");
+        return resourceBundleMessageSource;
+    }
 }
