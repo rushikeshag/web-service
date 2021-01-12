2a1cfb0ce40003b20a675987bbc19b6574212be3
Rushikesh Gore <rushikeshagore@gmail.com>
2021-01-12 22:36:54 +0530

    Swagger Configuration
    Swagger cofiguraiton for the details documentation of the API's

--- src/main/java/com/lotusstack/webservices/SwaggerConfig.java
+++ src/main/java/com/lotusstack/webservices/SwaggerConfig.java
@@ -4,17 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 public class SwaggerConfig {
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Webservices Documentation", "Application documentation", "1.0", "", "", "Apache", "");
+    private static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.any())
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(DEFAULT_API_INFO).select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .consumes(DEFAULT_PRODUCES_CONSUMES).produces(DEFAULT_PRODUCES_CONSUMES);
+//                .consumes();
     }
 }
--- src/main/java/com/lotusstack/webservices/db/User.java
+++ src/main/java/com/lotusstack/webservices/db/User.java
@@ -1,15 +1,21 @@
 package com.lotusstack.webservices.db;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "Description about the users")
 public class User {
 
     private Integer id;
-    @Size(min=2,message = "Name Should be atleast  2 character ")
+    @Size(min = 2, message = "Name Should be at least 2 character.")
     private String name;
     @Past
+    @ApiModelProperty(notes = "Birthdate should be in the past.")
     private Date dob;
 
     public User(Integer id, String name, Date dob) {
