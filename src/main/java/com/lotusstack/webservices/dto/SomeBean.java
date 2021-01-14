ebf29b282d60dd400e6ea2889990aff57f1d7320
Rushikesh Gore <rushikeshagore@gmail.com>
2021-01-14 12:58:41 +0530

    Filtering- Static Filtering
    Static filtering using Json Ignore annotation (@JsonIgnore at properties level and @JsonIgnoreProperties at class level)

---
+++ src/main/java/com/lotusstack/webservices/dto/SomeBean.java
@@ -0,0 +1,32 @@
+package com.lotusstack.webservices.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties("field1")
+public class SomeBean {
+
+    private String field1;
+    private String field2;
+    @JsonIgnore
+    private String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+}
